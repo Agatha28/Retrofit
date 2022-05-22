4d0b395e14ed66c00c5afa5899dc529b8285b3d2
Agatha28 <agasantoso7@gmail.com>
2022-05-22 23:06:08 +0700

    Retrofit

---
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.retrofit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.myrecyclerView)
+
+        val serviceGenerator = ServiceGenerator.buildService(ApiService::class.java)
+        val call = serviceGenerator.getPosts()
+
+
+
+        call.enqueue(object : retrofit2.Callback<MutableList<PostModel>> {
+            override fun onResponse(
+                call: Call<MutableList<PostModel>>, response: Response<MutableList<PostModel>>
+            ) {
+                if (response.isSuccessful) {
+                    recyclerView.apply {
+                        layoutManager = LinearLayoutManager(this@MainActivity)
+                        adapter = PostAdapter(response.body()!!)
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<MutableList<PostModel>>, t: Throwable) {
+                t.printStackTrace()
+                Log.e("error coy", t.message.toString())
+            }
+        })
+    }
+}
